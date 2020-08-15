0190cadef9048f67fdd18f5b49eb5d0fcf1cfd70
Tom Homewood <tomhomewood@gmail.com>
2020-08-15 11:32:52 +0100

    Added initial PoC for viewing a roster entry using LiveData and ViewModel.

---
+++ app/src/main/java/uk/tomhomewood/jmriroster/ActivityViewRosterEntry.kt
@@ -0,0 +1,67 @@
+package uk.tomhomewood.jmriroster
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import uk.tomhomewood.jmriroster.lib.v1.Result
+import uk.tomhomewood.jmriroster.lib.v1.Roster
+import uk.tomhomewood.jmriroster.lib.v1.RosterApi
+import uk.tomhomewood.jmriroster.lib.v1.RosterEntry
+
+class ActivityViewRosterEntry : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_roster_entry)
+
+        val rosterId = "30799"  //TODO Fetch from extras
+
+        val model: RosterEntryViewModel by viewModels {
+            RosterEntryViewModelFactory(rosterId)
+        }
+        model.getRosterEntry().observe(this, Observer<RosterEntry>{
+            rosterEntry -> findViewById<TextView>(R.id.roster_id).text = rosterEntry.name
+        })
+    }
+}
+
+class RosterEntryViewModelFactory(private val rosterId: String) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return RosterEntryViewModel(rosterId) as T
+    }
+}
+
+class RosterEntryViewModel(private val rosterId: String) : ViewModel() {
+
+    private val rosterApi: RosterApi by lazy {
+        RosterApi(BuildConfig.ROSTER_API_URL)
+    }
+
+    private val rosterEntryData: MutableLiveData<RosterEntry> by lazy {
+        MutableLiveData<RosterEntry>().also {
+            loadRosterEntry(rosterId)
+        }
+    }
+
+    private fun loadRosterEntry(id: String) {
+        viewModelScope.launch {
+            when (val response = rosterApi.getRosterEntry(id = id)){
+                is Result.Success -> {
+                    rosterEntryData.value = response.value.rosterEntry
+                }
+                is Result.Error -> {
+                    Log.e(TAG, "Error: "+response.message+", code: "+response.code)
+                }
+            }
+        }
+    }
+
+    fun getRosterEntry(): MutableLiveData<RosterEntry> {
+        return rosterEntryData
+    }
+}
